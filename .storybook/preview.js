51fe0a72284ec8f1cf08bd3e48f09490e110a752
Thiabut Lermytte <thibaut.lermytte@autodicar.fr>
2021-02-25 03:48:05 +0100

    Reset stack

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,15 +1,4 @@
-import '../styles/_globalStorybook.scss'
-import { addDecorator } from '@storybook/react'
-import { withInfo } from '@storybook/addon-info'
-import { addParameters } from '@storybook/react'
 
-addDecorator(withInfo)
-addParameters({
-  backgrounds: {
-    default: 'deep-blue',
-    values: [
-      { name: 'yellow', value: '#ffc205' },
-      { name: 'deep-blue', value: '#0a1726' },
-    ],
-  },
-})
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+}
---
+++ components/Landing/Header/index.stories.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import Header from './index';
+
+export default { title: 'Landing/Header' };
+
+export const header = () => <Header />;
