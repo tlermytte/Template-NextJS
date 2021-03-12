63b338289fb395ecb8cf18be6263640d47f30ff5
Thiabut Lermytte <thibaut.lermytte@autodicar.fr>
2021-03-12 13:07:14 +0100

    [MOD] import path based on baseUrl

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import { AppProps } from 'next/app';
-import '../styles/_globals.scss';
+import 'styles/_globals.scss';
 
 function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
